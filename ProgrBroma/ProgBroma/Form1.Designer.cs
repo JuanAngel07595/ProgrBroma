63b5950e4e6db6710aeac48f7ca6f4d26d0ee830
JuanAngel07595 <91165928+JuanAngel07595@users.noreply.github.com>
2025-03-02 00:04:54 -0500

    Versionamiento final #2

---
+++ ProgrBroma/ProgBroma/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace ProgBroma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnbroma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnbroma
+            // 
+            this.btnbroma.Location = new System.Drawing.Point(376, 298);
+            this.btnbroma.Margin = new System.Windows.Forms.Padding(4);
+            this.btnbroma.Name = "btnbroma";
+            this.btnbroma.Size = new System.Drawing.Size(196, 89);
+            this.btnbroma.TabIndex = 1;
+            this.btnbroma.Text = "Clickea Soldado";
+            this.btnbroma.UseVisualStyleBackColor = true;
+            this.btnbroma.Click += new System.EventHandler(this.btnbroma_Click);
+            this.btnbroma.MouseEnter += new System.EventHandler(this.btnbroma_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::ProgBroma.Properties.Resources.imgmilyardas;
+            this.ClientSize = new System.Drawing.Size(921, 551);
+            this.Controls.Add(this.btnbroma);
+            this.Name = "Form1";
+            this.Text = "Backsoldado";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnbroma;
+    }
+}
+
---
+++ ProgrBroma/ProgBroma/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ProgBroma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnbroma_Click(object sender, EventArgs e)
+        {
+
+        }
+        bool exte = true;
+        int A = 0;
+        int B = 0;
+        int postA = 0;
+        int postB = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnbroma_MouseEnter(object sender, EventArgs e)
+        {
+            exte = true;
+
+            while (exte)
+            {
+                A = random.Next(32, 400);
+
+                if (postA + 100 < A || postA - 100 < A)
+                {
+                    exte = false;
+                }
+            }
+
+            exte = true;
+
+            while (exte)
+            {
+                B = random.Next(35, 400);
+
+                if (postB + 100 < B || postB - 100 < B)
+                {
+                    exte = false;
+                }
+            }
+
+            btnbroma.Location = new Point(A, B);
+            postA = A;
+            postB = B;
+        }
+
+        Random random = new Random();
+    }
+}
+    
